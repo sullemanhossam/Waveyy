f3bb938538f62bd8b24e643f53ef3906c6c67c21
sullemanhossam <sullemanhossam4@gmail.com>
2024-11-18 17:11:07 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/your-repo-name',
+  basePath: '/Waveyy',
 };
 
 export default nextConfig;
